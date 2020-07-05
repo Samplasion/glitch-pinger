74a0ea86e16a5f320e495bc386b0284cca567b30
Francesco <samplasion@gmail.com>
2020-07-05 08:23:10 +0200

    Add user agents

--- worker.sh
+++ worker.sh
@@ -1,4 +1,4 @@
-wget https://plumbot.glitch.me/
-wget https://plumbeta.glitch.me/
-wget https://luigibeta.glitch.me/
-wget https://sapphire-stem.glitch.me/
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" "https://plumbot.glitch.me/"
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" "https://plumbeta.glitch.me/"
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" "https://luigibeta.glitch.me/"
+wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" "https://sapphire-stem.glitch.me/"
