b65830d66de578e2ffd3c05f74b0bff5f1dd1d21
Francesco <samplasion@gmail.com>
2020-08-06 23:36:01 +0200

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -2,6 +2,9 @@ call() {
     wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393" "$1" --no-check-certificate
 }
 
+# Samplasion
+call "https://tide-palm-pie.glitch.me/"
+
 # tee
 call "https://luigibeta.glitch.me/"
 call "https://sapphire-stem.glitch.me/"
